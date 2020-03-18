8890a5fd0c4ca9df4b27d23f2709eedaaa74d7d4
IonasNeonakis <ionas.neonakis@gmail.com>
2020-03-18 16:47:33 +0100

    début du projet

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let name;
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="bootstrap.min.css">
+</svelte:head>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<body>
+<button type="button" class="btn btn-primary">Primary</button>
+</body>
