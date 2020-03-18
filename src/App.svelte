879eba6cb9d2ded7caeeb00d25eead2f3cf0883d
IonasNeonakis <ionas.neonakis@gmail.com>
2020-03-18 19:38:30 +0100

    fin de la question 12

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,34 @@
 <script>
-	import HautDePage from './HautDePage.svelte';
+	import Input from './Input.svelte';
+
+	const regTel =/^([0-9][0-9] ?){5}$/ ; 
+
+	let prenompapa="";
+
+	function modifprenompapa(string){
+		prenompapa=string;
+	}
+
+	function telValidator(string){
+		return regTel.test(string);
+	}
 
 
-	function verif(name){
-		if (name.length<2){
+	function veriflongueur2(name){
+		if (name.length<2)
 			return false;
-		}else{
+		else
 			return true;
-		}
+		
 	};
 
 </script>
 
+<h1>Bonjour {prenompapa}</h1>
 
 
-<HautDePage placeholdernom={"Entrez un nom"} labelnom={"Nom"} verifierLongueur={verif}/>
+<Input placeholdervariable={"Entrez un nom"} labelvariable={"Nom"} verifierLongueur={veriflongueur2} modifTitle={modifprenompapa}/>
+
+<Input placeholdervariable={"Entrez un prenom"} labelvariable={"Prenom"} verifierLongueur={veriflongueur2} />
+
+<Input placeholdervariable={"Entrez un numero de Téléphone"} labelvariable={"N° téléphone"} verifierLongueur={telValidator} />
---
+++ src/Input.svelte
@@ -0,0 +1,39 @@
+<script>
+    //variable
+    export let variable="";
+    export let variableCorrect;
+    export let placeholdervariable;
+    export let labelvariable="variable";
+
+
+    export let verifierLongueur;
+    function verifier(){
+        variableCorrect=verifierLongueur(variable);
+    }
+
+    export let modifTitle;
+
+    function changerTitre(){
+        modifTitle(variable);
+    }
+
+    function verifetChangement(){
+        verifier();
+        changerTitre(variable);
+    }
+
+
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="bootstrap.min.css">
+</svelte:head>
+
+
+<div class="form-group">
+<label class="form-control-label" for="inputnom" id="nomLabel">{labelvariable}</label>
+<input id="inputnom" bind:value={variable}  on:keyup={verifetChangement} class="form-control {variableCorrect ? 'is-valid' : 'is-invalid' }"  placeholder="{placeholdervariable}">
+<div class="invalid-feedback">La longueur doit etre au moins de 2</div>
+</div>
+
+
